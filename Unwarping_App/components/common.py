8b90fb570fb2a9456f8487aac09ccb01bb6e3ae9
Kristina <69090314+kristina-arabov@users.noreply.github.com>
2026-02-08 18:59:21 -0500

    light toggle

--- Unwarping_App/components/common.py
+++ Unwarping_App/components/common.py
@@ -258,10 +258,11 @@ class DeviceRow(QWidget):
 
 
 class DevicesDropdown(QWidget):
-    def __init__(self, parent=None, camera=None):
+    def __init__(self, parent=None, camera=None, light_thread=None):
         super().__init__(parent)
 
         self.camera = camera
+        self.lights = light_thread
 
         self.setWindowFlags(Qt.Popup | Qt.FramelessWindowHint | Qt.NoDropShadowWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
@@ -318,12 +319,13 @@ class DevicesDropdown(QWidget):
 
         # Function calls
         self.row_camera.toggle.stateChanged.connect(lambda: device_service.toggle(self.row_camera, self.camera))
+        self.row_lights.toggle.stateChanged.connect(lambda: device_service.toggle(self.row_lights, self.lights))
 
 ######### End of Devices Dropdown #######################################################################################
 
 
 class Header(QWidget):
-    def __init__(self, stacked_widget, camera):
+    def __init__(self, stacked_widget, camera, lights):
         super().__init__()
 
         layout = QHBoxLayout()
@@ -333,6 +335,7 @@ class Header(QWidget):
 
         self.stacked = stacked_widget
         self.camera = camera
+        self.lights = lights
 
         self.legacy_btn = QPushButton("Legacy Mode", objectName="headerGrey")
         self.return_btn = QPushButton("Return", objectName="headerGrey")
@@ -363,7 +366,7 @@ class Header(QWidget):
 
     def showDevicesDropdown(self):
         if self.devices_dropdown is None:
-            self.devices_dropdown = DevicesDropdown(self, self.camera)
+            self.devices_dropdown = DevicesDropdown(self, self.camera, self.lights)
         
         button_pos = self.devices_btn.mapToGlobal(QPoint(-480, self.devices_btn.height()))
         self.devices_dropdown.move(button_pos)
--- Unwarping_App/services/device_service.py
+++ Unwarping_App/services/device_service.py
@@ -13,9 +13,10 @@ def toggle(row, device):
             pass
         # Lights
         case "lights":
-            pass
+            connect_lights(row, device) if row.toggle.isChecked() else disconnect_lights(row, device)
 
 
+''' CAMERA '''
 # Connect camera
 def connect_camera(row, camera, deviceIndex=0, resolution=(1280, 720), fps=None, name=None):
     camera.idx = deviceIndex
@@ -47,3 +48,26 @@ def disconnect_camera(row, camera):
 
     success = True if not camera.capture.isOpened() else False
 
+
+''' LIGHTS '''
+# Connect lights
+def connect_lights(row, lights):
+    lights.idx = row.port_combo.currentData()
+
+
+    if not lights.running:
+        try:
+            lights.start()
+            row.set_connected(True)
+        except:
+            pass
+
+    if lights.running:
+        row.set_connected(True)
+
+
+def disconnect_lights(row, lights):
+    if lights.running:
+        lights.stop()
+
+    row.set_connected(False)
