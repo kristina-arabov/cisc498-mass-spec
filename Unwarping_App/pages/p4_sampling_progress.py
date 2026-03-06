9d8d918059eae48c9fc24c86c4ce7ca326a04ef1
Kristina <69090314+kristina-arabov@users.noreply.github.com>
2026-03-06 18:16:12 -0500

    sampling pause front end

--- Unwarping_App/pages/p4_sampling_progress.py
+++ Unwarping_App/pages/p4_sampling_progress.py
@@ -10,6 +10,7 @@ from Unwarping_App.components.utils import generateProbeAcquisition, updatePixel
 
 class SamplingProgress(QWidget):
     next = pyqtSignal()
+    returnToConfig = pyqtSignal()
 
     def __init__(self):
         super().__init__()
@@ -33,26 +34,30 @@ class SamplingProgress(QWidget):
         right = QWidget()
         layout_right = QVBoxLayout(right)
 
-        img_loadingCircle = QLabel()
-        img_loadingCircle.setMovie(gif)
+        self.img_loadingCircle = QLabel()
+        self.img_loadingCircle.setMovie(gif)
         gif.start()
         gif.setScaledSize(QSize(100, 100))
 
-        label_points = QLabel("___ points sampled")
-        label_estimatedTime = QLabel("Estimated time left: ___")
+        self.label_points = QLabel("___ points sampled")
+        self.label_estimatedTime = QLabel("Estimated time left: ___")
 
-        button_stop = QPushButton("Stop", objectName="red")
+        self.button_pause = QPushButton("Pause", objectName="headerBlue")
+
+        self.operations = OperationButtons()
+        self.operations.hide()
 
         button_temp = QPushButton("Temporary next button", objectName="headerBlue")
         button_temp.clicked.connect(self.next.emit)
 
         layout_right.addStretch()
-        layout_right.addWidget(img_loadingCircle, alignment=Qt.AlignCenter)
+        layout_right.addWidget(self.img_loadingCircle, alignment=Qt.AlignCenter)
         layout_right.addStretch()
-        layout_right.addWidget(label_points, alignment=Qt.AlignCenter)
-        layout_right.addWidget(label_estimatedTime,alignment=Qt.AlignCenter)
+        layout_right.addWidget(self.operations)
+        layout_right.addWidget(self.label_points, alignment=Qt.AlignCenter)
+        layout_right.addWidget(self.label_estimatedTime,alignment=Qt.AlignCenter)
         layout_right.addStretch()
-        layout_right.addWidget(button_stop, alignment=Qt.AlignCenter)
+        layout_right.addWidget(self.button_pause)
         layout_right.addWidget(button_temp, alignment=Qt.AlignCenter)
         layout_right.addStretch()
 
@@ -63,4 +68,64 @@ class SamplingProgress(QWidget):
         layout.setContentsMargins(0, 0, 0, 0) 
         layout.setSpacing(0)  
 
+
+        ''' FUNCTIONS '''
+        self.button_pause.clicked.connect(lambda: self.handlePause(True))
+
+        self.operations.btn_resume.clicked.connect(lambda: self.handlePause(False))
+        self.operations.btn_abort.clicked.connect(lambda: self.stopSampling())
+
+    
+    def handlePause(self, stopped):
+        if stopped:
+            # TODO stop printer activity
+            self.img_loadingCircle.hide()
+            self.label_estimatedTime.hide()
+            self.label_points.hide()
+            self.button_pause.hide()
+
+            self.operations.show()
+        else:
+            self.img_loadingCircle.show()
+            self.label_estimatedTime.show()
+            self.label_points.show()
+            self.button_pause.show()
+
+            self.operations.hide()
+            # TODO resume sampling...
+
+    def stopSampling(self):
+        # TODO some printer command to stop fully... clear path?
+
+        # Reset as default
+        self.img_loadingCircle.show()
+        self.label_estimatedTime.show()
+        self.label_points.show()
+        self.button_pause.show()
+
+        self.operations.hide()
+
+        # Return to previous page (signal)
+        self.returnToConfig.emit()
+
         
+class OperationButtons(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout(self)
+
+        label = QLabel("Sampling Paused")
+        label.setStyleSheet("""
+            font-size: 20px;
+            font-weight: bold;
+        """)
+
+        self.btn_resume = QPushButton("Resume", objectName="blue")
+        self.btn_abort = QPushButton("Abort", objectName="red")
+
+        layout.addWidget(label, alignment=Qt.AlignCenter)
+        layout.addStretch()
+        layout.addWidget(self.btn_resume, alignment=Qt.AlignCenter)
+        layout.addWidget(self.btn_abort, alignment=Qt.AlignCenter)
+        layout.addStretch()
