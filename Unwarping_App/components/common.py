188ce155aa8d58c3813272c5ebe635d7eceb990a
Kristina <69090314+kristina-arabov@users.noreply.github.com>
2026-03-07 23:00:38 -0500

    rendering fixes

--- Unwarping_App/components/common.py
+++ Unwarping_App/components/common.py
@@ -1012,17 +1012,17 @@ class TagInformationSection(QWidget):
         container = QWidget(objectName="light_blue_box")
         layout_container = QVBoxLayout(container)
 
-        label_tagInformation = QLabel("Tag Information", objectName="larger")
+        label_tagInformation = QLabel("Probe-to-Tag Information", objectName="larger")
         label_tagInformation.setStyleSheet("font-weight: bold;")
 
         self.label_msg = QLabel("An AprilTag must be clearly visible in the image.")
-        self.label_msg.hide()
 
         ''' ROW 1 '''
         row_1 = QWidget()
         layout_row_1 = QHBoxLayout(row_1)
 
         label_bottomLeft = QLabel("Bottom-left corner")
+        label_bottomLeft.setStyleSheet("font-weight: bold;")
 
         label_bottomLeftX = QLabel("X: ")
         self.input_bottomLeftX = QLineEdit()
@@ -1043,6 +1043,8 @@ class TagInformationSection(QWidget):
         layout_row_2 = QHBoxLayout(row_2)
 
         label_tagSize = QLabel("Tag size (mm): ")
+        label_tagSize.setStyleSheet("font-weight: bold;")
+        
         self.input_tagSize = QLineEdit()
 
         layout_row_2.addWidget(label_tagSize, alignment=Qt.AlignLeft)
