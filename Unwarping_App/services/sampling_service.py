CS-139 2D-3D calculation fixes (#65)

Unwarping_App/services/sampling_service.py
@@ -95,7 +95,6 @@ def setTransformation(transformation, path, valid):
 
 
 
-# TODO fix bugginess ? why not working properly
 def findLocations(transformation, sampling, img):
     print("working!")
 
@@ -103,9 +102,10 @@ def findLocations(transformation, sampling, img):
     dot = img.dot
 
     sampling.originalLoc = transformation.photo_loc
+    print(sampling.originalLoc)
 
+    # If no reference point + ROI then don't caluclate locations
     if not dot or not rectangle:
-        print("NO DOT/ROI")
         return
 
     start_point = rectangle.topLeft()
@@ -124,14 +124,15 @@ def findLocations(transformation, sampling, img):
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-    mtx1 = transformation.mtx1
+    # bug here affecting actual transformation, make a copy
+    mtx1 = transformation.mtx1.copy()
     mtx1[0][0] = mtx1[0][0] * 0.01
     mtx1[1][1] = mtx1[1][1] * 0.01
 
     dist1 = np.array([[0,0,0,0,0]], dtype=np.float32) # Set to no distortion
 
-    mtx2 = transformation.mtx2
-    dist2 = transformation.dist2
+    mtx2 = transformation.mtx2.copy()
+    dist2 = transformation.dist2.copy()
 
 
     # Detect tag in the image
@@ -141,7 +142,7 @@ def findLocations(transformation, sampling, img):
 
     corners, ids, _ = detector.detectMarkers(image)
     if not corners or len(corners) == 0:
-        print("CANT DETECT TAG")
+        print("Tag not detected")
         return
 
     image_points = corners[0].reshape(-1, 2).astype(np.float32)
@@ -157,8 +158,6 @@ def findLocations(transformation, sampling, img):
 
     t = transformation.tag_bottom_left
     tag_corner = np.array([t[0], t[1], 0], dtype=np.float32)
-
-    print(tag_size, tag_corner)
 
 
     retval, rvec, tvec = cv2.solvePnP(object_points, image_points, mtx1, dist1, flags=cv2.SOLVEPNP_ITERATIVE)
@@ -182,34 +181,43 @@ def findLocations(transformation, sampling, img):
     t_base2cam = -R_cam2base_overlay.T @ t_cam2base_overlay
 
 
-    scale = 0.7 # TODO undo scale based on resolution
+    scale = img.scale_val
+    print(f"FIND LOCATIONS: {scale}")
 
     # PROCESS DOT ----------------------------------------------
-    sampling.dot = processDot(scale, transformation, dot, pos, R_cam2base_overlay)
+    sampling.dot = processDot(scale, transformation, dot, pos, R_cam2base_overlay, mtx1, mtx2, dist2)
     
 
     # PROCESS RECTANGLE --------------------------------------
-    sampling.rectangle = processRectangle(scale, transformation, rectangle, pos, R_cam2base_overlay)
+    sampling.rectangle = processRectangle(scale, transformation, rectangle, pos, R_cam2base_overlay, mtx1, mtx2, dist2)
 
 
-    print(vars(sampling))
+    # print(sampling.rectangle)
+    print(sampling.dot)
+    print(sampling.rectangle)
 
 
 
 # Function to get the 3D location of the reference point from a 2D location
-def processDot(scale, transformation, dot, pos, cam2base):
+def processDot(scale, transformation, dot, pos, cam2base, mtx1, mtx2, dist2):
     dot_unscaled = (int(dot.x() / scale), int(dot.y() / scale))
-    new_dot = calibration_service.undoSecondUnwarp(dot_unscaled, transformation.mtx2, transformation.dist2)
 
-    dot_from_cam_principal = getDirectionFromPixel(new_dot[0], new_dot[1], transformation.mtx1)
+    new_dot = calibration_service.undoSecondUnwarp(dot_unscaled, mtx2, dist2)
+
+    dot_from_cam_principal = getDirectionFromPixel(new_dot[0], new_dot[1], mtx1)
+
     dot_in_base = cam2base @ dot_from_cam_principal
 
     dot_x = pos[0] + (dot_in_base[0] * 10)
     dot_y = pos[1] + (dot_in_base[1] * 10)
     
     # Add probe offset to dot position
-    dot_x += transformation.offset_x
-    dot_y += transformation.offset_y
+    if transformation.offset_x < 0:
+        dot_x -= transformation.offset_x
+    else:
+        dot_x += transformation.offset_x
+    
+    dot_y -= transformation.offset_y
 
     probe_dot = [float(dot_x.item()), float(dot_y.item())]
 
@@ -217,18 +225,18 @@ def processDot(scale, transformation, dot, pos, cam2base):
 
 
 # Function to get the 3D range of the rectangle from 2D points
-def processRectangle(scale, transformation, rectangle, pos, cam2base):
+def processRectangle(scale, transformation, rectangle, pos, cam2base, mtx1, mtx2, dist2):
     start_point = rectangle.topRight()
     end_point = rectangle.bottomLeft()
 
     start_unscaled = (int(start_point.x() / scale), int(start_point.y() / scale))
     end_unscaled = (int(end_point.x() / scale), int(end_point.y() / scale))
 
-    start_point = calibration_service.undoSecondUnwarp(start_unscaled, transformation.mtx2, transformation.dist2)
-    end_point = calibration_service.undoSecondUnwarp(end_unscaled, transformation.mtx2, transformation.dist2)
+    start_point = calibration_service.undoSecondUnwarp(start_unscaled, mtx2, dist2)
+    end_point = calibration_service.undoSecondUnwarp(end_unscaled, mtx2, dist2)
 
-    start_point_from_cam_principal = getDirectionFromPixel(start_point[0], start_point[1], transformation.mtx1)
-    end_point_from_cam_principal = getDirectionFromPixel(end_point[0], end_point[1], transformation.mtx1)
+    start_point_from_cam_principal = getDirectionFromPixel(start_point[0], start_point[1], mtx1)
+    end_point_from_cam_principal = getDirectionFromPixel(end_point[0], end_point[1], mtx1)
 
     start_point_in_base = cam2base @ start_point_from_cam_principal
     end_point_in_base = cam2base @ end_point_from_cam_principal
@@ -237,15 +245,23 @@ def processRectangle(scale, transformation, rectangle, pos, cam2base):
     start_x = pos[0] + (start_point_in_base[0] * 10)
     start_y = pos[1] + (start_point_in_base[1] * 10)
 
-    start_x += transformation.offset_x
-    start_y += transformation.offset_y
+    if transformation.offset_x < 0:
+        start_x -= transformation.offset_x
+    else:
+        start_x += transformation.offset_x
+    
+    start_y -= transformation.offset_y
 
     # 3D end position
     end_x = pos[0] + (end_point_in_base[0] * 10)
     end_y = pos[1] + (end_point_in_base[1] * 10)
 
-    end_x += transformation.offset_x
-    end_y += transformation.offset_y
+    if transformation.offset_x < 0:
+        end_x -= transformation.offset_x
+    else:
+        end_x += transformation.offset_x
+    
+    end_y -= transformation.offset_y
 
 
     probe_rectangle = [float(end_x.item()), float(end_y.item()), float(start_x.item()), float(start_y.item())]